62e6709100cefa6ce5626aeaf29d132eb38f7cd7
Marilynn Stone <hello@marilynnstone.com>
2022-11-08 02:36:41 +0000

    Add code to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    # Store all emails in lowercase to avoid duplicates and case-sensitive login errors.
+    @user.email.downcase!  
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmations)
+  end
+  
 end
