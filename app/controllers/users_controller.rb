d03cc860c94e325f1113b8457ddc012d47c8a74e
Marilynn Stone <hello@marilynnstone.com>
2022-11-09 02:44:57 +0000

    Update users controller for flash.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,15 @@ class UsersController < ApplicationController
     # Store all emails in lowercase to avoid duplicates and case-sensitive login errors.
     @user.email.downcase!  
     
+    if @user.save
+      # If user saves in the db successfully:
+      flash[:notice] = "Account created successfully!"
+      redirect_to root_path
+    else
+      # If user fails model validation - probably a bad password or duplicate email:
+      flash.now.alert = "Couldn't create account. Please make sure you are using a valid email and password, and then try again."
+      render :new
+    end
 
   end
 
