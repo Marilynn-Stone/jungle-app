a438712f0245a338fb987573b168dbf87ea68191
Marilynn Stone <hello@marilynnstone.com>
2022-11-15 17:35:51 +0000

    Create product validation tests. Pass tests.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,31 +2,45 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    @category = Category.new(name: "category")
-    @product = Product.new(name: "product", price: 12.34, quantity: 5, category: @category.name)
-    
+
+    it 'creates a product' do
+      @category = Category.new(name: "Category")
+      @category.save
+      @product = Product.new(name: "product", price: 12.34, quantity: 5, category_id: @category.id)
+      expect(@product).to be_valid
+      expect(@product.errors.full_messages).to_not be_present
+    end
+
     it 'is not valid without a name' do
-      @product.name = nil
+      @category = Category.new(name: "category")
+      @category.save
+      @product = Product.new(price: 12.34, quantity: 5, category_id: @category.id)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
+
     it 'is not valid without a price' do
-      @product.price = nil
+      @category = Category.new(name: "category")
+      @category.save
+      @product = Product.new(name: "product", quantity: 5, category_id: @category.id)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
+
     it 'is not valid without a quantity' do
-      @product.quantity = nil
+      @category = Category.new(name: "category")
+      @category.save
+      @product = Product.new(name: "product", price: 12.34, category_id: @category.id)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
+
     it 'is not valid without a category' do
-      @product.category = nil
+      @category = Category.new(name: "category")
+      @category.save
+      @product = Product.new(name: "product", price: 12.34, quantity: 5)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
-      
-      
-      
-      # @category = Category.new
-      # @product = Product.new
-      # expect(@product).to have_attributes(:name, :price, :quantity, :category)
-    # end
   end
 end
