83ce474303362814e6a0a207c48935e95011fc48
Marilynn Stone <hello@marilynnstone.com>
2022-11-17 08:41:08 -0800

    Test products load and are the correct number.

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,4 +2,12 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
